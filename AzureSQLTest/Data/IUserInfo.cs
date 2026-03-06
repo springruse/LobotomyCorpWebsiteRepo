f5fdc92b6690fa2c72ef60f8a6b161e652f3286e
springruse <152939330+springruse@users.noreply.github.com>
2026-03-05 18:58:41 -0700

    Added UserInfoDatabase Services + Use on Blazor.

---
+++ AzureSQLTest/Data/IUserInfo.cs
@@ -0,0 +1,12 @@
+﻿using AzureSQLTest.Models;
+
+namespace AzureSQLTest.Data
+{
+    public interface IUserInfo
+    {
+        public List<UserInfo> GetUserInfos();
+        public void AddUser(UserInfo user);
+        public void DeleteUser(int id);
+        public void UpdateUser(UserInfo user);
+    }
+}
---
+++ AzureSQLTest/Models/UserInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AzureSQLTest.Models
+{
+    [Table("UserInfo", Schema = "dbo")]
+    public class UserInfo
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public bool Admin { get; set; }
+
+    }
+}
